aa0f0a72625ace96b6d1b33fe16708d466dd9bad
dawberry <dhailu128@gmail.com>
2022-09-19 21:15:11 +0300

    python firstday

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-def write():
-    sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-    exit(1)
-write()
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)

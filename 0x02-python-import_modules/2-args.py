8da359126d5bd56947ab1f1e2612b8518bda1d7f
Dawberry <dhailu128@gmail.com>
2022-09-22 22:47:23 +0300

    python import module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-import sys
-print("{} argumets:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    
+    argCount = len(sys.argv) - 1
+
+    if argCount == 0:
+        print("0 arguments.")
+    elif argCount == 1:
+        print("1 argument:")
+    else:
+        print("{} argumetns:".format(argCount))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
-print(sum)
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+
+    for i in range(1, len(sys.argv) - 1):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))

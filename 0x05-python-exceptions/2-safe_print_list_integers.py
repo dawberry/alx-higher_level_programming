d0ec32f947c4a1c928c1a4d68134af8bd6283ecf
dawberry <dhailu128@gmail.com>
2022-10-11 13:46:09 +0300

    python exception handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,13 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             int(my_list[i])
             print("{}".format(my_list[i]), end="")
             size += 1
-        except:
+        except (ValueError, TypeError):
             i += 1
     print("")
     return size
-
-lis = [1,2,34,5,9]
-print("{}".format( safe_print_list_integers(lis, 3)))
-
-lis = [1, 3, 4, "dave", 10]
-print("{}".format(safe_print_list_integers(lis, 7)))

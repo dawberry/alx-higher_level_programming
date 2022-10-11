526d5a433bbaedfbb632d38145bb45e3d2ea00e2
dawberry <dhailu128@gmail.com>
2022-10-11 19:18:01 +0300

    python object oriented learning

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/pyhton3
+"""Defines Square"""
+
 
 class Square:
+    """
+    Private attribute: size
+    """
+
     def __init__(self, size):
+        """
+        initalizes size attribute
+        """
+
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
+"""
+Define a Square
+"""
+
 
 class Square:
+    """
+    Private attribute: size
+    """
+
     def __init__(self, size=0):
-        try:
-            if not isinstance(size, int):
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        """
+        initalizes size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
+    """
+    Returns area of Square
+    """
+
     def area(self):
-        return self. __size ** 2
+        return self.__size ** 2

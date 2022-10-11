e9876b98b659bd8b94725820849206733120f820
dawberry <dhailu128@gmail.com>
2022-10-11 19:11:01 +0300

    python object oriented learning

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,14 +8,9 @@ class Square:
     def __init__(self, size=0):
         """Initalizes attribute size"""
 
-        try:
-            if not isinstance(size,  int):
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size

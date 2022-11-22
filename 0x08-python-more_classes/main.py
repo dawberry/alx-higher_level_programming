d77d2035332dc499fa080ed97fcddcfa2b412be0
dawberry <dhailu128@gmail.com>
2022-11-22 11:03:31 +0300

    python more classes

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-Rectangle = __import__("0-rectangle").Rectangle
+Rectangle = __import__("1-rectangle").Rectangle
 
 new_rect = Rectangle()
 
-print(type(new_rect))
-print(new_rect.__dict__)
+print(new_rect.width)
+new_rect.width = 20
+print(new_rect.width)
